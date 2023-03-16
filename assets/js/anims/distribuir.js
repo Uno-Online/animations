43c14a4a8f635d017ecf948b674b35f1916488d8
drelocatelli <andressalocatelli@hotmail.com>
2023-03-16 17:13:05 -0300

    card reveal

--- assets/js/anims/distribuir.js
+++ assets/js/anims/distribuir.js
@@ -6,16 +6,35 @@ cardsEl.forEach((card, i) => {
 });
 
 let timeline = gsap.timeline();
-    timeline.fromTo('.cards img', {delay: .8, rotation: 12, x: '-120px', stagger: .5}, {delay: .8, rotation: 0, x: 0, ease: 'back', duration: 1, stagger: .5, onComplete: () => revealCard()});
-    
+    timeline.fromTo('.cards img', {delay: .8, rotation: -8, zIndex:0, x: '-130px', stagger: .5}, {delay: .8, rotation: 0, x: 0, y: '-20px', ease: 'back', duration: 1, stagger: .5, onComplete: () => revealCard()});
+let timelineMainCard = gsap.timeline();
+    timelineMainCard.to('.cards img:nth-last-child(2)', {duration: 1.5, ease: 'circ', x: '80px', y: '-68px', rotation: "5deg"});
+    timelineMainCard.to('.cards img:nth-last-child(2)', {duration: 1.5, ease: 'circ', x: '150px', rotation: '20deg'});
+    timelineMainCard.to('.cards img:nth-last-child(2)', {duration: .5, ease: 'circ', zIndex: 2});
+    timelineMainCard.to('.cards img:nth-last-child(2)', {duration: 1.5, ease: 'back', x: 0, y: '-20px', rotation: 0, });
+    timelineMainCard.fromTo('.cards img:nth-last-child(3)', {delay: .8}, { zIndex: 2, duration: 1 , onComplete: () => removeAllCards()});
+timelineMainCard.pause();
+
 timeline.play();
     
 function revealCard() {
     let randomNum = Math.floor(Math.random() * 7) + 1;
     const color = game.colors[Math.floor(Math.random() * game.colors.length - 1) + 1];
     const lastSecondCard = cardsEl[cardsEl.length - 2];
+    timelineMainCard.resume();
     lastSecondCard.src = `assets/img/cards/${randomNum}_card_${color}.png`;
-    gsap.fromTo('.cards img:nth-last-child(2)', {delay: .8, opacity: 0, filter: 'brightness(0%)'}, { opacity: 1, zIndex: 2, duration: 1, filter: 'brightness(100%)'});
+    
+}
+function removeAllCards() {
+    cardsEl.forEach((card, i) => {
+        if(i != cardsEl.length - 2) {
+            card.style.transition = 'opacity 1.5s';
+            card.style.opacity = 0;
+            setTimeout(() => {
+                card.remove();
+            }, 1000);
+        }
+    });
 }
 
 reloadBtn.onclick = () => {
