827e053994f656faef62296b203ab67496466dfa
drelocatelli <andressalocatelli@hotmail.com>
2023-03-16 17:15:02 -0300

    card reveal

--- assets/js/anims/distribuir.js
+++ assets/js/anims/distribuir.js
@@ -9,7 +9,7 @@ let timeline = gsap.timeline();
     timeline.fromTo('.cards img', {delay: .8, rotation: -8, zIndex:0, x: '-130px', stagger: .5}, {delay: .8, rotation: 0, x: 0, y: '-20px', ease: 'back', duration: 1, stagger: .5, onComplete: () => revealCard()});
 let timelineMainCard = gsap.timeline();
     timelineMainCard.to('.cards img:nth-last-child(2)', {duration: 1.5, ease: 'circ', x: '80px', y: '-68px', rotation: "5deg"});
-    timelineMainCard.to('.cards img:nth-last-child(2)', {duration: 1.5, ease: 'circ', x: '150px', rotation: '20deg'});
+    timelineMainCard.to('.cards img:nth-last-child(2)', {duration: .5, ease: 'circ', x: '150px', rotation: '20deg'});
     timelineMainCard.to('.cards img:nth-last-child(2)', {duration: .5, ease: 'circ', zIndex: 2});
     timelineMainCard.to('.cards img:nth-last-child(2)', {duration: 1.5, ease: 'back', x: 0, y: '-20px', rotation: 0, });
     timelineMainCard.fromTo('.cards img:nth-last-child(3)', {delay: .8}, { zIndex: 2, duration: 1 , onComplete: () => removeAllCards()});
