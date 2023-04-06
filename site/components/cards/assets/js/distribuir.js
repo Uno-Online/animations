fbfcdd7a76cefa369046a70b78581e9d0ee3a709
drelocatelli <andressalocatelli@hotmail.com>
2023-04-06 18:37:48 -0300

    animated cards

--- site/components/cards/assets/js/distribuir.js
+++ site/components/cards/assets/js/distribuir.js
@@ -7,12 +7,15 @@
     cards.forEach((card, i) => {
         card.style.setProperty('--i', (i+1));
         card.style.setProperty('--numCards', cards.length);
-        card.classList.add('uno');
         card.classList.add('card');
         userBoard.appendChild(card);
         
         
         //fx
+        setTimeout(() => {
+            card.classList.add('uno');
+        }, 500)
+        
         setTimeout(() => {
             card.src = originalCards[i].src;
             card.classList.remove('uno');
