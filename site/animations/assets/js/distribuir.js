539311df851f14029ec014919e570720e2a9de5a
drelocatelli <andressalocatelli@hotmail.com>
2023-04-07 15:26:48 -0300

    anim

--- site/animations/assets/js/distribuir.js
+++ site/animations/assets/js/distribuir.js
@@ -11,6 +11,7 @@ global.reactive.gameStarted.subscribe((value) => {
 class Animations {
     static getCardTopMax() {
         const calculateVh = (width) => {
+            if(width == 1024) return -30;
             const ratio = width / global.screens.fullHd.width;
             return Math.round(-35 * ratio);
         }
@@ -29,14 +30,14 @@ class Animations {
         target.style.setProperty('transition', 'none');
             timeline.to(target, {duration: 0, rotation: '0deg', transform: 'none'})
             timeline.to(target, {duration: .2, top: this.getCardTopMax(), left: '140px', ease: 'circ', position: 'fixed', zIndex: global.reactive.cardIdx.getValue()})
-            // timeline.to(target, {duration: .2, opacity:0, pointerEvents: 'none', onComplete: () => {addCartToMain()}});
+            timeline.to(target, {duration: .2, opacity:0, pointerEvents: 'none', onComplete: () => {addCartToMain()}});
         const timeline2 = gsap.timeline({paused: true});
             timeline2.to(target, {opacity: '0', top:0, left:0, position: 'relative'});
             timeline2.to(target, {duration: .2, ease: 'circ', opacity: 1})
         function addCartToMain() {
             timeline2.resume();
-            middle.append(target);
             const middleCardsQuantity = middle.children.length;
+            middle.append(target);
             // remove early card added 
             if(middleCardsQuantity >= 2) {
                 middle.children[0].remove();
