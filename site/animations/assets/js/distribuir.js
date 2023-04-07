87cced113c5244b891c6a23dd84581b15a32ebd8
drelocatelli <andressalocatelli@hotmail.com>
2023-04-07 12:11:55 -0300

    fx

--- site/animations/assets/js/distribuir.js
+++ site/animations/assets/js/distribuir.js
@@ -1,1 +1,26 @@
-const reactive = rxjs;
+cardIdx$.subscribe((value) => console.log(value));
+
+gameStarted$.subscribe((value) => {
+    if (value) {
+        const cards = document.querySelectorAll('.cards.cards-curved .deck img');
+        for (let card of cards) {
+            card.addEventListener('click', (e) => {
+                Animations.userCardAnimation(e.target);
+                cardIdx$.setState(cardIdx$.getValue() + 1);
+            });
+        }
+    }
+});
+
+class Animations {
+    static userCardAnimation(target) {
+        const timeline = gsap.timeline();
+        timeline.to(target, { top: '-320px', left: '160px', rotation: '0deg', zIndex: cardIdx$.getValue() });
+        timeline.to(target, {
+            transform: 'none!important',
+            onComplete: () => {
+                target.style.transform = 'none';
+            },
+        });
+    }
+}
